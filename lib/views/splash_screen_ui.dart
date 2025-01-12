bf2904b2e058c3a42c69ff69b8f1254793c07e9f
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-01-12 11:34:51 +0700

    v.3 แก้ไข splash ui นะจ๊ะ

--- lib/views/splash_screen_ui.dart
+++ lib/views/splash_screen_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iot_sau_pm_project/views/signin_ui.dart';
 
 class SplashScreenUI extends StatefulWidget {
   const SplashScreenUI({super.key});
@@ -9,9 +10,54 @@ class SplashScreenUI extends StatefulWidget {
 
 class _SplashScreenUIState extends State<SplashScreenUI> {
   @override
+  void initState() {
+    //โค้ดหน่วงเวลาหน้าจอ
+    Future.delayed(
+      //หน่วงเวลา
+      Duration(seconds: 3),
+      //เมื่อครบเวลาให้เปิดไปหน้า SigninUI แบบย้อนกลับไม่ได้
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => SigninUI(),
+        ),
+      ),
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red[900],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: MediaQuery.of(context).size.width * 0.5,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.025,
+            ),
+            Text(
+              'IoT SAU PM Thailand',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: MediaQuery.of(context).size.height * 0.025,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.025,
+            ),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
